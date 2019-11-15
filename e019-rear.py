1c4e7d92d4890e31242de1936654df4efdebae2d
Ren-L-H <55141419+Ren-L-H@users.noreply.github.com>
2019-11-15 14:44:52 +0800

    Update e019-rear.py
    For some reason there returned the error "to many vales to unpack" in the function "result", and there was a problem with the right reading in of "small_dataset", which might be caused because I just copyed and pasted the code.
    The proposed two changes, though perhaps not pretty, are supposed to be able to fix the problems above.
    Thank you for your code~ (Though I didn't find the code for Python 3, which is required for my work. But the differences can be easily dealed with.) 
    I think the hard part of this problem is the math.

--- e019-rear.py
+++ e019-rear.py
@@ -86,29 +86,29 @@ def reversal_distance(a, b):
 def result(s):
     pairs = s.strip().split("\n\n")
     pairs = [handle_pair(p) for p in pairs]
-
-    for a, b in pairs:
-        reversal_distance(a, b)
+    print(pairs)
+    for i in pairs:
+        reversal_distance(i[0],i[1])
 
 
 if __name__ == "__main__":
 
     small_dataset = """
-                    1 2 3 4 5 6 7 8 9 10
-                    3 1 5 2 7 4 9 6 10 8
+1 2 3 4 5 6 7 8 9 10
+3 1 5 2 7 4 9 6 10 8
 
-                    3 10 8 2 5 4 7 1 6 9
-                    5 2 3 1 7 4 10 8 6 9
+3 10 8 2 5 4 7 1 6 9
+5 2 3 1 7 4 10 8 6 9
 
-                    8 6 7 9 4 1 3 10 2 5
-                    8 2 7 6 9 1 5 3 10 4
+8 6 7 9 4 1 3 10 2 5
+8 2 7 6 9 1 5 3 10 4
 
-                    3 9 10 4 1 8 6 7 5 2
-                    2 9 8 5 1 7 3 4 6 10
+3 9 10 4 1 8 6 7 5 2
+2 9 8 5 1 7 3 4 6 10
 
-                    1 2 3 4 5 6 7 8 9 10
-                    1 2 3 4 5 6 7 8 9 10
-                    """
+1 2 3 4 5 6 7 8 9 10
+1 2 3 4 5 6 7 8 9 10
+"""
     # large_dataset = open('datasets/rosalind_rear.txt').read().strip()
 
     result(small_dataset)
